dd9fb5826d7cb0530979fac7fb80d127ec071730
Susan Curtis <scwmbg@yahoo.com>
2017-11-08 15:34:38 -0500

    Moved business info and passed info from App to child components.

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,40 +1,24 @@
 import React from 'react';
 import './Business.css';
 
-class Business extends React.Component
-{
-  constructor() {
-    super()
-    this.business = {
-      imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-      name: 'MarginOtto Pizzeria',
-      address: '1010 Paddington Way',
-      city: 'Flavortown',
-      state: 'NY',
-      zipCode: '10101',
-      category: 'Italian',
-      rating: 4.5,
-      reviewCount: 90
-  }
-}
-
+class Business extends React.Component {
   render() {
      return(
      <div className="Business">
       <div className="image-container">
-        <img src={this.business.imageSrc} alt='' />
+        <img src={this.props.business.imageSrc} alt='' />
     </div>
-  <h2>{this.business.name}</h2>
+  <h2>{this.props.business.name}</h2>
   <div className="Business-information">
     <div className="Business-address">
-      <p>{this.business.address}</p>
-      <p>{this.business.city}</p>
-      <p>{this.business.state} {this.business.zipCode}</p>
+      <p>{this.props.business.address}</p>
+      <p>{this.props.business.city}</p>
+      <p>{this.props.business.state} {this.props.business.zipCode}</p>
     </div>
     <div className="Business-reviews">
-      <h3>{this.business.category}</h3>
-      <h3 className="rating">{this.business.rating}</h3>
-      <p>{this.business.reviewCount}</p>
+      <h3>{this.props.business.category}</h3>
+      <h3 className="rating">{this.props.business.rating}</h3>
+      <p>{this.props.business.reviewCount}</p>
     </div>
   </div>
 </div>
