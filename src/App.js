83be118f04c4e579636a8388e1c8c2a07991b0ec
Susan Curtis <scwmbg@yahoo.com>
2017-11-14 16:19:55 -0500

    Completed changes for part three to searchbar.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,6 @@ import './App.css';
 import BusinessList from './components/BusinessList/BusinessList.js';
 import SearchBar from './components/SearchBar/SearchBar.js';
 
-
-/*constructor() {
-  super()
-  this.business = {*/
-
   const business = {
     imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
     name: 'MarginOtto Pizzeria',
@@ -30,36 +25,19 @@ import SearchBar from './components/SearchBar/SearchBar.js';
 
 
 class App extends React.Component {
+  searchYelp(term, location, sortBy) {
+    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+  }
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
-        <SearchBar />
+        <SearchBar searchYelp={this.searchYelp} />
         <BusinessList businesses={businesses} />
      </div>
     )
   }
 }
 
-/*import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-        Hello Codecademy!
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
-*/
 export default App;
