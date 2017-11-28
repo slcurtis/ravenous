5f55cf60b067dae243d7ced47ae8e1da267e33d8
slcurtis <scwmbg@yahoo.com>
2017-11-28 11:42:34 -0500

    Modifications.

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -5,16 +5,37 @@ let accessToken = '';
 
 const Yelp = {
   getAccessToken() {
-    if (accessToken) {
+    if(accessToken) {
       return new Promise(resolve => resolve(accessToken));
     }
-      return fetch(`https://cors-anywhere.herokuapp.com/api.yelp.com/oauth2/token?grant_type=client_credentials&client_id=${clientId}&client_secret=${secret}`), {
-        {method: 'POST',
-      }).then(response => {
-        return response.json();
-      }).then(jsonResponse => {
-        accessToken = jsonResponse.access_token;
-      })
-  }
-},
-}
+      return fetch(`https://cors-anywhere.herokuapp.com/api.yelp.com/oauth2/token?grant_type=client_credentials&client_id=${clientId}&client_secret=${secret}`, {
+        method: 'POST'}).then(response => response.json()).then(jsonResponse => accessToken = jsonResponse.access_token);
+    },
+
+  search(term, location, sortBy) {
+    //eslint-disable-next-line
+    return Yelp.getAccessToken().then(() => fetch('https://cors-anywhere.herokuapp.com/api.yelp.com/v3/businesses/search?term=${term}location=${location}&sort_by=${sortBy}', {
+      headers: {Authorization:`Bearer ${accessToken}`}
+    })
+    .then(response => {
+    return response.json(); })
+    .then(jsonResponse => {
+      if (jsonResponse.businesses) {
+        return jsonResponse.businesses.map(business => ({
+          id: business.id,
+          imageSrc: business.image_url,
+          name: business.name,
+          address: business.location.address1,
+          city: business.location.city,
+          state: business.location.state_code,
+          zipCode: business.location.postal_code,
+          category: business.categories[0].title,
+          rating: business.rating,
+          reviewCount: business.review_count
+        }));
+      }
+    }),
+  )
+ }
+};
+export default Yelp;
